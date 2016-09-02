c15fbbd2607cb3aa2c2c89806f8cdbe69bd75103
MaxGura <mgura@students.mchenry.edu>
2016-09-02 00:11:23 -0500

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,20 @@
+# Variables
+person = "Chris Pratt"
+pet = "dinosaur"
+color = "black"
+example = "eggs"
+place = "forest"
+group = "InGen"
+person2 = "J.D."
+adverb = "rapidly"
+action = "jogs"
+
+# poem
+print(person + " , " " the magic " + pet + " lived by the sea ")
+print(" And " + action + " in the " + color + place)
+print(" in a land called Honnah Lee ")
+print(group + " loved that rascal " + person2)
+print(" And brought him " + pet + example + " and sealing wax and other fancy stuff, oh! ")
+
+
+
